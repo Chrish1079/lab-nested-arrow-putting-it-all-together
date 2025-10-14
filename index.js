3c99de333119a8102d99b5cf10a600b33dcd4adb
Chris1079 <itssuper1079@gmail.com>
2025-10-14 19:00:46 -0400

    submit

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-
-
+const userInfo = {
+username: 'sam',
+password: '1234'
+};
+function createLoginTracker(userInfo) {
+let attemptCount = 0; 
+return function(passwordAttempt) {
+  attemptCount++;
+  if (passwordAttempt === userInfo.password) {
+    return "Login successful";
+  }
+  if (attemptCount > 3) {
+     return "Account locked due to too many failed login attempts";
+  }
+  return `Attempt ${attemptCount}: Login failed`;
+};
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
